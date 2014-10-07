7c17df19082ed2fa9d4ac258c698dae6e15e1ec3
Davis Gossage <dcg13@duke.edu>
2014-10-06 20:32:25 -0400

    Formatting.  You won't get me this time checkstyle!

--- src/parser/CommandFactory.java
+++ src/parser/CommandFactory.java
@@ -1,64 +1,64 @@
 package parser;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Locale;
 import java.util.ResourceBundle;
 import commands.Command;
 
+
 public class CommandFactory {
-    
+
     private static ResourceBundle ourCommandClasses;
     private static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
     private static final String DOT = ".";
-    
-    public CommandFactory() {
-        ourCommandClasses = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE+"ClassConversion");
+
+    public CommandFactory () {
+        ourCommandClasses = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "ClassConversion");
     }
 
-    
     /**
-     * parses command with getCommand method to get type of command  
+     * parses command with getCommand method to get type of command
+     * 
      * @param command
      * @return parsed command
      */
-    public Command getCommand(Command command){
-		return command;
+    public Command getCommand (Command command) {
+        return command;
     }
-    
+
     /**
      * adds a command to the queue for processing
      * 
      * @param command
      */
-    public void addCommand(String command){
-        //command should be in form 'Forward 50'
-        String[] commandParams = command.split(" "); 
+    public void addCommand (String command) {
+        // command should be in form 'Forward 50'
+        String[] commandParams = command.split(" ");
         String commandBundleName = ourCommandClasses.getString("command_package");
         String commandClassName = ourCommandClasses.getString(commandParams[0]);
-        Command commandInstance = instantiateClassFromString(commandBundleName+DOT+commandClassName,Double.parseDouble(commandParams[1]));
-        
-    }
+        Command commandInstance =
+                instantiateClassFromString(commandBundleName + DOT + commandClassName,
+                                           Double.parseDouble(commandParams[1]));
 
+    }
 
     /**
-     * cycles through the queue of command objects, 
+     * cycles through the queue of command objects,
      * and determines the order for processing
      */
-    public void processQueuedCommands(){
-        
+    public void processQueuedCommands () {
+
     }
-    
-    
+
     private Command instantiateClassFromString (String commandClassName, double arg) {
         try {
-            Class<?> commandClass =  Class.forName(commandClassName);
+            Class<?> commandClass = Class.forName(commandClassName);
             return (Command)commandClass.getConstructors()[0].newInstance(arg);
         }
-        catch (ClassNotFoundException | IllegalArgumentException | SecurityException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            // TODO Auto-generated catch block
+        catch (ClassNotFoundException | IllegalArgumentException | SecurityException
+                | InstantiationException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
             return null;
         }
     }
-    
+
 }
