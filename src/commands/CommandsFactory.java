1f45347b80af5a0a41084ac9ab1e8d20f138009f
Davis Gossage <dcg13@duke.edu>
2014-10-25 22:26:35 -0400

    We no longer use command classes, they are handled by the javacc parser

--- src/commands/CommandsFactory.java
+++ src/commands/CommandsFactory.java
@@ -9,15 +9,11 @@ import MovementAndImageAPI.src.TurtleHandler;
 
 public class CommandsFactory {
 
-    private static ResourceBundle ourCommandClasses;
     private static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
     private static final String DOT = ".";
-    private List<Command> myCommandList;
     private TurtleHandler myTurtleHandler;
 
     public CommandsFactory () {
-        myCommandList = new ArrayList<Command>();
-        ourCommandClasses = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "ClassConversion");
     }
     
     public void setTurtleHandler(TurtleHandler turtleHandler){
@@ -39,16 +35,6 @@ public class CommandsFactory {
   public ICommand CommandExecute(String commandName) {
       return new CommandExecute(commandName);
   }
-   
-    /**
-     * parses command with getCommand method to get type of command
-     * 
-     * @param command
-     * @return parsed command
-     */
-    public Command getCommand (Command command) {
-        return command;
-    }
 
     public ICommand pickUpTurtle() {
         return new ActivityCommand(myTurtleHandler, ActivityCommand.CommandType.PICK_UP);
@@ -113,42 +99,6 @@ public class CommandsFactory {
     
     public ICommand clearScreen(){
         return new MovementCommand(myTurtleHandler, MovementCommand.CommandType.CLEAR_SCREEN, null);
-    }
-    
-    /**
-     * adds a command to the queue for processing
-     * 
-     * @param command
-     */
-    public void addCommand (String command) {
-        // command should be in form 'Forward 50'
-        String[] commandParams = command.split(" ");
-        String commandBundleName = ourCommandClasses.getString("command_package");
-        String commandClassName = ourCommandClasses.getString(commandParams[0]);
-        Command commandInstance =
-                instantiateClassFromString(commandBundleName + DOT + commandClassName,
-                                           Double.parseDouble(commandParams[1]));
-        myCommandList.add(commandInstance);
-    }
-
-    /**
-     * cycles through the queue of command objects,
-     * and determines the order for processing
-     */
-    public void processQueuedCommands () {
-
-    }
-
-    private Command instantiateClassFromString (String commandClassName, double arg) {
-        try {
-            Class<?> commandClass = Class.forName(commandClassName);
-            return (Command)commandClass.getConstructors()[0].newInstance(arg);
-        }
-        catch (ClassNotFoundException | IllegalArgumentException | SecurityException
-                | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
 }
